4829888394c6065cb0168ac21f7c0e182a08d5d1
Shivam Kumar <shivamraj6136@gmail.com>
2022-12-26 20:59:12 +0530

    Shivam_chatBot

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 5050
 const cors = require('cors')
+const path = require('path')
+const fp = path.join(__dirname, "/public")
+// console.log(fp)
+
 const { WebhookClient } = require('dialogflow-fulfillment')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(fp))
 
 app.get('/', async (req, resp) => {
-    resp.json({ msg: "hello from server" })
+    resp.send("index.html")
 })
 
 app.post('/', async (req, res) => {
@@ -18,9 +23,13 @@ app.post('/', async (req, res) => {
     let intentMap = new Map()
 
     const demo = async (agent) => {
-        agent.add("sending respnse from backend")
+        agent.add("sending respnse from backendx")
+    }
+    const demo2 = async (agent) => {
+        agent.add("KNVDNDKJJKD")
     }
     intentMap.set("webhook", demo)
+    intentMap.set("Ambuja", demo2)
     agent.handleRequest(intentMap)
 
 })
